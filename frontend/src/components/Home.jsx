0544b770c8320856f2efdf472f5095702c0680ab
MuditaBhathar <mudita.bhathar205@gmail.com>
2025-08-06 00:26:07 +0530

    backend to frontend render

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -14,7 +14,7 @@ export default function Home() {
     setResult(null)
 
     try {
-      const res = await fetch('http://localhost:5000/api/improve', {
+      const res = await fetch('https://site-content-optimizer.onrender.comgit ad', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ url })
