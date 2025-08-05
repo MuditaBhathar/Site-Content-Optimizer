538797eaaced8dbf4780f29c97ab894cfbb1ffad
Mudita Bhathar <120955114+MuditaBhathar@users.noreply.github.com>
2025-08-06 00:36:23 +0530

    Update Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -14,7 +14,7 @@ export default function Home() {
     setResult(null)
 
     try {
-      const res = await fetch('https://site-content-optimizer.onrender.comgit ad', {
+      const res = await fetch('https://site-content-optimizer.onrender.com', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ url })
